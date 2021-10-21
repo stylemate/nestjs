29221bf6b4e4d3fc5d3460d180639ae8a3ed484c
ORMEEHYUNGKEUN CHA <orum93@naver.com>
2021-10-21 17:19:18 +0900

    setup

--- di-practice/src/main.ts
+++ di-practice/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { ComputerModule } from './computer/computer.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    ComputerModule,
+    new FastifyAdapter({ logger: true }),
+  );
   await app.listen(3000);
 }
 bootstrap();
