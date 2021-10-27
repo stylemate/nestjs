1ee44a35464b05ebcd2090561cdc71646c03a1bf
ORMEEHYUNGKEUN CHA <orum93@naver.com>
2021-10-27 10:05:52 +0900

    DI finished

--- di-practice/src/cpu/cpu.module.ts
+++ di-practice/src/cpu/cpu.module.ts
@@ -4,6 +4,7 @@ import { PowerModule } from 'src/power/power.module';
 
 @Module({
     imports: [PowerModule], //module is imported and has access to all its services
-    providers: [CpuService]
+    providers: [CpuService],
+    exports: [CpuService]
 })
 export class CpuModule {}
--- di-practice/src/cpu/cpu.service.ts
+++ di-practice/src/cpu/cpu.service.ts
@@ -4,7 +4,10 @@ import { PowerService } from 'src/power/power.service';
 @Injectable()
 export class CpuService {
     //Whenever CpuService is created, PowerService will be created
-    constructor(private powerService: PowerService) {
-
+    constructor(private powerService: PowerService) {}
+    compute(a: number, b: number) {
+        console.log('Drawing 10 watts of power from Power Service');
+        this.powerService.supplyPower(10);
+        return a + b;
     }
 }
