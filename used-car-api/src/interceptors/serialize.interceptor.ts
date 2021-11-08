71520e0f8a91b6ef5df1f3fdba0f70a60049b95a
ORMEEHYUNGKEUN CHA <orum93@naver.com>
2021-11-08 23:46:23 +0900

    user dto & interceptor

--- used-car-api/src/interceptors/serialize.interceptor.ts
+++ used-car-api/src/interceptors/serialize.interceptor.ts
@@ -6,18 +6,18 @@ import {
 } from '@nestjs/common';
 import { Observable, map } from 'rxjs';
 import { plainToClass } from 'class-transformer';
+import { UserDto } from 'src/users/dtos/user.dto';
 
 export class SerializeInterceptor implements NestInterceptor {
     intercept(
         context: ExecutionContext,
         next: CallHandler<any>
     ): Observable<any> | Promise<Observable<any>> {
-        //runs before request is handled
-        console.log('running before the handler:', context);
         return next.handle().pipe(
             map((data: any) => {
-                //runs before the response is sent
-                console.log('running before response', data);
+                return plainToClass(UserDto, data, {
+                    excludeExtraneousValues: true
+                });
             })
         );
     }
