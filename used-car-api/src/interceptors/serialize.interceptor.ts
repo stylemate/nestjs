f0e6b62f7b699d45cb9bb461c7e894ea108bcec8
ORMEEHYUNGKEUN CHA <orum93@naver.com>
2021-11-08 23:50:19 +0900

    interceptor accepts dto argument

--- used-car-api/src/interceptors/serialize.interceptor.ts
+++ used-car-api/src/interceptors/serialize.interceptor.ts
@@ -1,21 +1,20 @@
 import {
-    UseInterceptors,
     NestInterceptor,
     ExecutionContext,
     CallHandler
 } from '@nestjs/common';
 import { Observable, map } from 'rxjs';
 import { plainToClass } from 'class-transformer';
-import { UserDto } from 'src/users/dtos/user.dto';
 
 export class SerializeInterceptor implements NestInterceptor {
+    constructor(private dto: any) {}
     intercept(
         context: ExecutionContext,
         next: CallHandler<any>
     ): Observable<any> | Promise<Observable<any>> {
         return next.handle().pipe(
             map((data: any) => {
-                return plainToClass(UserDto, data, {
+                return plainToClass(this.dto, data, {
                     excludeExtraneousValues: true
                 });
             })
